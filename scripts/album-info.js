48fe2dbd2df46e6530aab92420f16d5f673078b8
Claudia Romano <cromano31415@gmail.com>
2017-10-15 14:37:22 -0500

    added rest of user story

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
 	$('#album-title').text(album.title);
 	$('img#album-cover-art').attr('src', album.albumArtUrl);
+//we need artist name in .artist
+	$('.artist').text(album.artist);
+//we need general release info in #release-info would be text too inside a div
+	$('#release-info').text(album.releaseInfo);
+
 });
